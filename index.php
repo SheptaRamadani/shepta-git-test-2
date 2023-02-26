1a397b8e13b807c0bec3c14852b798f8ac2b45e4
SheptaRamadani <ramadanishepta@gmail.com>
2023-02-26 19:31:08 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
 	<h1>Hello World!</h1>
+	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore perferendis vero omnis expedita consequuntur sint est, dicta commodi. Rerum debitis voluptas voluptatibus incidunt consectetur facilis vel culpa iure nulla repellendus.</p>
 </body>
 </html>
